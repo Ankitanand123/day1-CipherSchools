287326aadeac5d9e698835f7adaaa87a171c7b90
Ankitanand123 <117056428+Ankitanand123@users.noreply.github.com>
2024-08-05 20:18:52 +0530

    Create assignment.cpp

---
+++ assignment.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter 1st number ";
+    cin>>a;
+    cout<<"Enter 2nd number ";
+  cin>>b;
+    cout<<"The Sum of both the numbers are "<<a+b<<endl;
+    cout<<"The difference of both the numbers are "<<a-b<<endl;
+    cout<<"The multiplication of both the numbers are "<<a*b<<endl;
+    cout<<"The Division of both the numbers are "<<a/b<<endl;
+    cout<<"The modulous of both the numbers are "<<a%b<<endl;
+    
+    return 0;
+}
+
