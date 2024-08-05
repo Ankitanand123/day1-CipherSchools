5f4d9876d3e2818a59ce1a05ceef21ad3ec776de
Ankitanand123 <117056428+Ankitanand123@users.noreply.github.com>
2024-08-05 20:15:33 +0530

    Create inclass.cpp

---
+++ inclass.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // cout<<"Hello world"<<endl;
+    // int a =20;
+    // cout<<a<<endl;
+    // cout<<sizeof(a);
+
+    int a,b;
+    cout<<"Enter 1st number ";
+    cin>>a;
+    cout<<"Enter 2nd number ";
+  
+    cin>>b;
+   
+    cout<<"The Sum of numbers are "<<a+b;
+    return 0;
+}
+
